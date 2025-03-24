8c06b9d3e694a7e7507771272f17495282d638b2
pricelenoir <price.lenoir@gmail.com>
2025-03-24 12:54:03 -0400

    Tested macroscopic

--- src/macroscopic.cpp
+++ src/macroscopic.cpp
@@ -31,6 +31,12 @@ void macroscopic(Domain &domain, Constants &constants) {
             derivativeY(node, domain, &Node::uX, &Node::dudy);
             derivativeX(node, domain, &Node::uY, &Node::dvdx);
             derivativeY(node, domain, &Node::uY, &Node::dvdy);
+        }
+    }
+
+    for (long i = 0; i < domain.nX; i++) {
+        for (long j = 0; j < domain.nY; j++) {
+            node = domain.nodes[i][j];
 
             node->pStar = 0;
             node->phi = 0;
@@ -58,6 +64,11 @@ void macroscopic(Domain &domain, Constants &constants) {
 
             // Calculate density from composition
             node->rho = (rho1 * (node->phi - phi2) + rho2 * (phi1 - node->phi)) / (phi1 - phi2);
+            node->nu = (nu1 * (node->rho - rho2) + nu2 * (rho1 - node->rho)) / (rho1 - rho2);
+            node->p = node->pStar * node->rho / 3;
+
+            // Calculate intermediate values for force calculation below
+            node->tmp = node->rho * node->nu * (node->dudx + node->dvdy + node->dvdx + node->dudy);
         }
     }
 
@@ -72,18 +83,13 @@ void macroscopic(Domain &domain, Constants &constants) {
             node->mu = node->mu0 - k * node->d2phidx2;
             node->mu = 2.2 * node->mu;
 
-            node->nu = (nu1 * (node->rho - rho2) + nu2 * (rho1 - node->rho)) / (rho1 - rho2);
-            node->p = node->pStar * node->rho / 3;
-
-            // Calculate intermediate values for force calculation below
-            node->tmp = node->rho * node->nu * (node->dudx + node->dvdy + node->dvdx + node->dudy);
             derivativeX(node, domain, &Node::tmp, &Node::forceX);
             derivativeY(node, domain, &Node::tmp, &Node::forceY);
 
             /// Calculate force (forceX and forceY currently hold intermediate gradient values)
-            node->forceX = gX * node->rho + node->forceX - (1/3) * node->pStar * (rho1 - rho2) * node->dphidx + node->mu * node->dphidx;
-            node->forceY = gY * node->rho + node->forceY - (1/3) * node->pStar * (rho1 - rho2) * node->dphidy + node->mu * node->dphidy;
-            
+            node->forceX = gX * node->rho + node->forceX - (1.0/3.0) * node->pStar * (rho1 - rho2) * node->dphidx + node->mu * node->dphidx;
+            node->forceY = gY * node->rho + node->forceY - (1.0/3.0) * node->pStar * (rho1 - rho2) * node->dphidy + node->mu * node->dphidy;
+
             // Adjusting pressure and velocities based on Lee paper suggestions (macroscopic equations)
             node->uX = node->uX + (0.5 * node->forceX) / node->rho;
             node->uY = node->uY + (0.5 * node->forceY) / node->rho;
